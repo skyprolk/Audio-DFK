5633a108010742015f020b45f609b719ea388feb
Jonathan Fly <163408+JonathanFly@users.noreply.github.com>
2023-05-21 18:28:56 -0400

    missed files

---
+++ bark_infinity/hubert/hubert_manager.py
@@ -0,0 +1,39 @@
+import os.path
+import shutil
+import urllib.request
+
+import huggingface_hub
+
+
+class HuBERTManager:
+    @staticmethod
+    def make_sure_hubert_installed():
+
+        print(f"Current working directory: {os.getcwd()}")
+  
+        install_dir = os.path.join('/home/jon/mamba_projects/bark_infinity_cloning/bark/bark_infinity','hubert')
+        print(f"Install dir: {install_dir}")
+        if not os.path.isdir(install_dir):
+            os.mkdir(install_dir)
+
+        install_file = os.path.join(install_dir, 'hubert.pt')
+        download_url = 'https://dl.fbaipublicfiles.com/hubert/hubert_base_ls960.pt'
+        if not os.path.isfile(install_file):
+            print('Downloading HuBERT small model')
+            urllib.request.urlretrieve(download_url, install_file)
+            print('Downloaded HuBERT')
+
+
+    @staticmethod
+    def make_sure_tokenizer_installed():
+        install_dir = os.path.join('/home/jon/mamba_projects/bark_infinity_cloning/bark/bark_infinity','hubert')
+        if not os.path.isdir(install_dir):
+            os.mkdir(install_dir)
+        install_file = os.path.join(install_dir, 'tokenizer.pth')
+        repo = 'GitMylo/bark-voice-cloning'
+        file = 'quantifier_hubert_base_ls960_14.pth'
+        if not os.path.isfile(install_file):
+            print('Downloading HuBERT custom tokenizer')
+            huggingface_hub.hf_hub_download(repo, file, local_dir=install_dir, local_dir_use_symlinks=False)
+            shutil.move(os.path.join(install_dir, file), install_file)
+            print('Downloaded tokenizer')
