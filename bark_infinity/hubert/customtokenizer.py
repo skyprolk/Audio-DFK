da2f12f470bd5023159f0bd623079e560ad3a4b8
KNOiT <67490509+knoit-sky@users.noreply.github.com>
2023-07-23 10:50:45 +0530

    Made compatible to CPU

--- bark_infinity/hubert/customtokenizer.py
+++ bark_infinity/hubert/customtokenizer.py
@@ -123,7 +123,7 @@ class CustomTokenizer(nn.Module):
                 data_from_model.output_size,
                 data_from_model.version,
             )
-        model.load_state_dict(torch.load(path))
+        model.load_state_dict(torch.load(path,map_location=map_location))
         if map_location:
             model = model.to(map_location)
         return model
